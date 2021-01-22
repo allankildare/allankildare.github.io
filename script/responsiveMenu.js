4956e80c4ea29439e542d2abc1cdbb0908a89639
Allan Kildare <allankildare27@gmail.com>
2021-01-22 20:53:24 -0300

    + menu resposivo

---
+++ script/responsiveMenu.js
@@ -0,0 +1,8 @@
+function collapse(el) {
+    $(el).click(function() {
+        $(this).toggleClass('active')
+        $('.menu').toggleClass('active')
+    })
+}
+$(document).ready(collapse('.burger-icon'))
+$(document).ready(collapse('.menu li a'))
