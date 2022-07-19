6eb56dd700e190420ddd05362553253ce1ebffd2
Allan Kildare <allankildaredev@gmail.com>
2022-07-18 22:52:14 -0300

    fix: enable navigation between other pages
    - external links not can be used

--- script/app.js
+++ script/app.js
@@ -1,7 +1,8 @@
 $('nav a').click(function(e) {
     e.preventDefault()
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top
+    var id = $(this).attr('href')
+    if(id.includes('#')) targetOffset = $(id).offset().top
+    else window.location.href = id        
     $('html, body').animate({
         scrollTop: targetOffset - 30
     }, 600)
