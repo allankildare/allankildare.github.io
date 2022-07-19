0d446144b82bc4e3d585039c6d5e17bc3125202c
Allan Kildare <allankildaredev@gmail.com>
2022-07-18 23:12:12 -0300

    refactor: just removing whitespace

--- script/app.js
+++ script/app.js
@@ -1,8 +1,8 @@
 $('nav a').click(function(e) {
     e.preventDefault()
     var id = $(this).attr('href')
-    if(id.includes('#')) targetOffset = $(id).offset().top
-    else window.location.href = id        
+        if(id.includes('#')) targetOffset = $(id).offset().top
+        else window.location.href = id        
     $('html, body').animate({
         scrollTop: targetOffset - 30
     }, 600)
